266ba4c968c7f1e662389db0992acf9220da0c37
Ethanol <ethanolchennault@gmail.com>
2021-03-20 19:38:39 -0700

    removed form on home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Loading from '~/client/components/common/Loading'
-import { Upload } from '~/client/components/Upload'
+import Container from '~/client/components/common/Container'
 
 export default function Home() {
   return (
-    <div>
-      <Upload />
-    </div>
+    <Container col>
+      <p className="mt-4 text-white text-6xl">Welcome!</p>
+      <a
+        className="text-4xl mt-4 underline text-app-light-blue-2"
+        href="https://github.com/3than0ls/stoodint"
+      >
+        Check out the unfinished GitHub repo of this project.
+      </a>
+    </Container>
   )
 }
